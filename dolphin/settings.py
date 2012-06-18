d196c8cffb827919702cc537ec0b7e0c104c52c7
Jeremy Self <jeremy.self@coxinc.com>
2012-06-18 11:15:31 -0400

    Add stats of redis model

---
+++ dolphin/settings.py
@@ -0,0 +1,11 @@
+from django.conf import settings as django_settings
+
+DOLPHIN_USE_REDIS = getattr(django_settings, 'DOLPHIN_USE_REDIS', False)
+DOLPHIN_LIMIT_TO_SESSION = getattr(django_settings, 'DOLPHIN_LIMIT_TO_SESSION', True)
+DOLPHIN_STORE_FLAGS = getattr(django_settings, 'DOLPHIN_STORE_FLAGS', True)
+DOLPHIN_USE_GIS = getattr(django_settings, 'DOLPHIN_USE_GIS', True)
+
+DOLPHIN_REDIS_HOST = getattr(django_settings, 'DOLPHIN_REDIS_HOST', 'localhost')
+DOLPHIN_REDIS_PORT = getattr(django_settings, 'DOLPHIN_REDIS_PORT', 6379)
+DOLPHIN_SET_NAME = getattr(django_settings, 'DOLPHIN_SET_NAME', 'featureflags')
+DOLPHIN_REDIS_TEST_DB = getattr(django_settings, "DOLPHIN_REDIS_TEST_DB", 'featureflag_test')
