45ffdc69fe71b3c113dfbf7db10edb86052c7e39
Jeremy Self <jeremy.self@coxinc.com>
2012-06-26 14:21:48 -0400

    Update readme

--- docs/make_readme.sh
+++ docs/make_readme.sh
@@ -1,4 +1,6 @@
+CURDIR=`pwd`
 cd build/text
-cat index.txt usage.txt custom_tests.txt setup.txt settings.txt
+cat index.txt usage.txt custom_tests.txt setup.txt settings.txt > readme.txt
 sed ':a;N;$!ba;s/Contents.*Usage/Usage/g' -i readme.txt
 mv readme.txt ../../../README
+cd $CURDIR
