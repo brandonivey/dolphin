eef85a310bf8d1fc7cb6867201ad98c638262ff7
Jeremy Self <jeremy.self@coxinc.com>
2012-06-29 15:48:48 -0400

    Remove redis tests if not using redis

--- dolphin/tests/__init__.py
+++ dolphin/tests/__init__.py
@@ -1,7 +1,9 @@
+from dolphin import settings
 from .testutils import SetActiveTest
 from .flipper import (ActiveTest, UserFlagsTest, GeoIPTest, ABTest, CustomFlagTest,
                       SessionTest)
 from .templatetags import ActiveTagTest, FlagListTest
 from .middleware import RequestStoreMiddlewareTest
-from .redis import (RedisActiveTest, RedisUserFlagsTest, RedisGeoIPTest,
-                    RedisABTest, RedisCustomFlagTest, RedisActiveTagTest, RedisFlagListTest)
+if settings.DOLPHIN_USE_REDIS:
+    from .redis import (RedisActiveTest, RedisUserFlagsTest, RedisGeoIPTest,
+                        RedisABTest, RedisCustomFlagTest, RedisActiveTagTest, RedisFlagListTest)
